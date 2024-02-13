6d8794f4575544e36f91e3df240d47ab055f0a5e
Sangavi Kumar <sangavitommy@gmail.com>
2024-02-13 23:44:30 +0530

    Added files to the repo

---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, request, flash, redirect
+import pickle
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from werkzeug.utils import secure_filename
+import tensorflow_hub as hub
+import os
+app = Flask(__name__)
+
+model = load_model(('models/brain.h5'), custom_objects={'KerasLayer': hub.KerasLayer})
+
+def model_predict(img_path, model):
+    test_image = image.load_img(img_path, target_size = (224,224))
+    test_image = image.img_to_array(test_image)
+    test_image = test_image / 255
+    test_image = np.expand_dims(test_image, axis=0)
+    result = model.predict(test_image)
+    result
+
+
+    if result <= 0.5:
+        result = "The Person has no Brain Tumor"
+    else:
+        result = "The Person has Brain Tumor"
+
+    return result
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/malaria", methods=['GET', 'POST'])
+def malariaPage():
+    return render_template('malaria.html')
+
+@app.route("/pneumonia", methods=['GET', 'POST'])
+def pneumoniaPage():
+    return render_template('pneumonia.html')
+
+@app.route("/skin", methods=['GET', 'POST'])
+def skinPage():
+    return render_template('skin.html')
+
+
+@app.route('/brain', methods=['GET'])
+def brainPage():
+    return render_template('brain.html')
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route("/prediction", methods = ['POST', 'GET'])
+def predictPage():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+    except:
+        message = "Please enter valid Data"
+        return render_template("home.html", message = message)
+
+    return render_template('prediction.html', pred = pred)
+
+@app.route("/malariapredict", methods = ['POST', 'GET'])
+def malariapredictPage():
+    if request.method == 'POST':
+        try:
+            if 'image' in request.files:
+                img = Image.open(request.files['image'])
+                img = img.resize((36,36))
+                img = np.asarray(img)
+                img = img.reshape((1,36,36,3))
+                img = img.astype(np.float64)
+                model = load_model("models/malaria.h5")
+                pred = np.argmax(model.predict(img)[0])
+        except:
+            message = "Please upload an Image"
+            return render_template('malaria.html', message = message)
+    return render_template('malaria_predict.html', pred = pred)
+
+@app.route("/pneumoniapredict", methods = ['POST', 'GET'])
+def pneumoniapredictPage():
+    if request.method == 'POST':
+        try:
+            if 'image' in request.files:
+                img = Image.open(request.files['image']).convert('L')
+                img = img.resize((36,36))
+                img = np.asarray(img)
+                img = img.reshape((1,36,36,1))
+                img = img / 255.0
+                model = load_model("models/pneumonia.h5")
+                pred = np.argmax(model.predict(img)[0])
+        except:
+            message = "Please upload an Image"
+            return render_template('pneumonia.html', message = message)
+    return render_template('pneumonia_predict.html', pred = pred)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result = preds
+        return result
+    return None
+
+if __name__ == '__main__':
+	app.run(debug = True)   
